80be218df7f6a43760446ab9599a4fa21d6b396f
AJII GODSWILL <godswillajii2025@gmail.com>
2026-06-24 17:52:19 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length){
+            return false
+        }
+
+        const notebook1 = {}
+        const notebook2 = {}
+
+        for(let i=0;i< s.length;i++) {
+            const letter1 = s[i]
+            const letter2 = t[i]
+
+            notebook1[letter1] = (notebook1[letter1] || 0) + 1
+            notebook2[letter2] = (notebook2[letter2] || 0) + 1
+        } 
+
+        for (let letter in notebook1) {
+            if (notebook1[letter] !== notebook2[letter]) {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
